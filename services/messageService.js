8d504ec4368d380183752c569ff0ccfed22905fe
dani1kx <its.dani1kx@gmail.com>
2026-04-09 21:51:29 +0300

    hotfix

--- services/messageService.js
+++ services/messageService.js
@@ -3,7 +3,11 @@ const pool = require("../db/db-manager");
 let messageService = {
     getAllByChatId: async (chatId) => {
         const CreateQuery = `
-           // Тут треба дописати запит
+            SELECT messages.id, messages.text, messages.created, messages.user_id, messages.chat_id, users.username
+            FROM messages
+            JOIN users ON users.id = messages.user_id
+            WHERE messages.chat_id = $1
+            ORDER BY messages.created ASC
         `;
         
         const result = await pool.query(query, [chatId]);
